dfca357dc4c9142f2e31a1deb7c56c94a999255e
motedyn <motedyn@motedyn-vm.(none)>
2014-09-16 12:37:53 -0600

    Publicacion 2 de app en modulo notas

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="ercss/inicio.css">
     <script type="text/javascript" src="js/jquery.js"></script> 
     <script type="text/javascript" src="js/bootstrap.js"></script>
-    <title>El Redentor</title>
+    <title>El Redentor1</title>
     
 </head>
 <body id="body2">
